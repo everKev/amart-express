ffa27e74078f5f3fed840ab4a663574477760062
Kevin Everrett <keverrett1989@gmail.com>
2023-09-01 14:26:35 -0400

    Hero Banner

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,18 +1,22 @@
 'use client';
 import React from 'react';
+import Image from 'next/image';
+import { CustomButtonProps } from '@/types';
 
-const CustomButton = () => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
   return (
-    <div>
-      <button
-        disabled={false}
-        type={'button'}
-        className={`custom-btn`}
-        onClick={() => {}}
-      >
-        <span className={`flex-1`}></span>
-      </button>
-    </div>
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}> {title} </span>
+    </button>
   );
 };
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,31 @@
+'use client';
 import Image from 'next/image';
 import { CustomButton } from '@/components';
 
 const Hero = () => {
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Start your day with A-Mart Xpress - faster than ever!
+          Start your day with <span className="hero__sub">A-Mart Express</span>-
+          faster than ever!
         </h1>
         <p className="hero__subtitle">
           Streamline your morning routine with the a-mart experience to get you
-          in and on your way.{' '}
+          in and on your way.
         </p>
-        <CustomButton />
+        <CustomButton
+          title="Show Now"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/coffee.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay" />
+        </div>
       </div>
     </div>
   );
